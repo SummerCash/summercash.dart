d5d1d30d79ff8b2f295f5ccbcbaab3a2b9492f2a
Dowland Aiello <mitsukomegumii@gmail.com>
2019-05-16 21:33:04 -0400

    lib/src, test: Upgraded tests.

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -1,13 +1,70 @@
+import 'dart:typed_data';
+import 'package:summercash/src/api_exception.dart';
+import 'package:tuple/tuple.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
+import 'dart:io';
+import 'dart:convert';
+
 /// API abstractions for the common package.
 class Common {
-  /// Node https API endpoint.
+  /// Base node http API endpoint (does not include package in path).
+  String _baseEndpoint;
+
+  /// Node http API endpoint.
   String _endpoint;
+
+  /// API gateway HTTP client.
+  IOClient _client;
 
   /// Initialize an instance of the common API.
   Common(String endpoint) {
-    this._endpoint = endpoint; // Set endpoint
+    var wrapped = new HttpClient(); // Initialize HTTP client
+    wrapped.badCertificateCallback =
+        (X509Certificate cert, String host, int port) =>
+            true; // Skip verify certificate
+
+    this._client = new IOClient(wrapped); // Set HTTP client
+    this._baseEndpoint = endpoint; // Set base endpoint
+    this._endpoint = endpoint + '/twirp/common.Common'; // Set endpoint
   }
+
+  /// Destroy common API client.
+  void destroy() {
+    _client.close(); // Close client
+  }
+
+  /// Get base API endpoint.
+  String get baseEndpoint => _baseEndpoint;
 
   /// Get API endpoint.
   String get endpoint => _endpoint;
+
+  /// Get method endpoint.
+  String methodEndpoint(String method) {
+    return _endpoint +
+        '/${method[0].toUpperCase()}${method.substring(1)}'; // Return method endpoint
+  }
+
+  /// Encode given byte array to hex-formatted, MemPrefix-compliant byte array.
+  Future<Uint8List> encode(Uint8List b) async {
+    final response = await _client.post(methodEndpoint('Encode'),
+        body: json.encode({'input': new Base64Codec().encode(b)}),
+        headers: {
+          'Content-Type': 'application/json'
+        }); // Make post request, get response
+
+    final jsonDecoded = json.decode(response.body); // Decode JSON
+
+    if (response.body.contains('"code":"internal"')) {
+      // Check for errors
+      throw new APIException(jsonDecoded['msg']); // Throw an API Exception
+    }
+
+    final stringEncoded =
+        jsonDecoded['message'].toString().replaceAll('\n', ''); // Remove \n
+
+    return new Uint8List.fromList(
+        stringEncoded.codeUnits); // Encode to byte array
+  }
 }
--- test/summercash_test.dart
+++ test/summercash_test.dart
@@ -1,5 +1,18 @@
+import 'dart:typed_data';
+
 import 'package:test/test.dart';
+import 'package:summercash/src/common.dart';
 
 void main() {
-  group('A group of tests', () {});
+  group('common tests', () {
+    Common common =
+        new Common('https://localhost:8080'); // Initialize common API
+
+    test('encoded test value should match runtime-calculated value', () async {
+      expect(
+          await common.encode(new Uint8List.fromList('test'.codeUnits)),
+          equals(new Uint8List.fromList(
+              '0x74657374'.codeUnits))); // Test encode functionality
+    });
+  });
 }
