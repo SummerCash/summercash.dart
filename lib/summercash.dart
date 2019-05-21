93b0eccdae3c55f0f3842621922c74ceaa269392
Dowland Aiello <mitsukomegumii@gmail.com>
2019-05-20 21:28:39 -0400

    lib: Exported API refs.

--- lib/summercash.dart
+++ lib/summercash.dart
@@ -5,6 +5,8 @@ export 'src/common_api.dart'; // The common wrapper.
 export 'src/accounts_api.dart'; // The accounts wrapper.
 export 'src/chain_api.dart'; // The chain wrapper.
 export 'src/chain_config_api.dart'; // The chain config wrapper.
+export 'src/crypto_api.dart'; // The crypto wrapper.
+export 'src/transactions_api.dart'; // The transactions wrapper.
 
 export 'src/account.dart'; // The account wrapper.
 export 'src/chain.dart'; // The chain wrapper.
